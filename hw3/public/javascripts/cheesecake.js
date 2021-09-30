ad4250db3022cebe58cb3cdd22930b344458d06f
madison2023 <78463010+madison2023@users.noreply.github.com>
2021-09-29 20:31:05 -0700

    hw 5

--- hw3/public/javascripts/cheesecake.js
+++ hw3/public/javascripts/cheesecake.js
@@ -6,8 +6,11 @@ $(document).ready(function(){
   function changeDropDownText(month) {
     $("#dropDownButton").text(month);
     //post data from server to the list with last month's orders
-    $.post('/orders', function(data) {
+    $.post('/orders', {month: month}, function(data) {
+      //I couldn't get the correct data to show up here :(
       const orders = JSON.parse(data);
+      console.log(orders[0].quantity);
+
       //editing each bullet point
       $("ul li:eq(0)").text(orders[0].quantity + " " + orders[0].topping);
       $("ul li:eq(1)").text(orders[1].quantity + " " + orders[1].topping);
@@ -35,17 +38,17 @@ $(document).ready(function(){
   });
 
   //changes the dropdown text when an element is clicked
-  $('#Jan').click(function(){ changeDropDownText("Jan"); });
-  $('#Feb').click(function(){ changeDropDownText("Feb"); });
-  $('#Mar').click(function(){ changeDropDownText("Mar"); });
-  $('#Apr').click(function(){ changeDropDownText("Apr"); });
-  $('#May').click(function(){ changeDropDownText("May"); });
-  $('#Jun').click(function(){ changeDropDownText("Jun"); });
-  $('#Jul').click(function(){ changeDropDownText("Jul"); });
-  $('#Aug').click(function(){ changeDropDownText("Aug"); });
-  $('#Sep').click(function(){ changeDropDownText("Sep"); });
-  $('#Oct').click(function(){ changeDropDownText("Oct"); });
-  $('#Nov').click(function(){ changeDropDownText("Nov"); });
-  $('#Dec').click(function(){ changeDropDownText("Dec"); });
+  $('#Jan').click(function(){ changeDropDownText("JAN"); });
+  $('#Feb').click(function(){ changeDropDownText("FEB"); });
+  $('#Mar').click(function(){ changeDropDownText("MAR"); });
+  $('#Apr').click(function(){ changeDropDownText("APR"); });
+  $('#May').click(function(){ changeDropDownText("MAY"); });
+  $('#Jun').click(function(){ changeDropDownText("JUN"); });
+  $('#Jul').click(function(){ changeDropDownText("JUL"); });
+  $('#Aug').click(function(){ changeDropDownText("AUG"); });
+  $('#Sep').click(function(){ changeDropDownText("SEP"); });
+  $('#Oct').click(function(){ changeDropDownText("OCT"); });
+  $('#Nov').click(function(){ changeDropDownText("NOV"); });
+  $('#Dec').click(function(){ changeDropDownText("DEC"); });
 
 });
--- hw3/routes/orders.js
+++ hw3/routes/orders.js
@@ -2,6 +2,7 @@
 
 var express = require('express');
 var router = express.Router();
+var dbms_promise = require('./dbms_promise');
 
 //CODE FOR SPECIFICATION 1
 
@@ -20,8 +21,11 @@ var router = express.Router();
 
 router.post('/', function(req, res, next) {
     //creating data to post
+    var selectedMonth = "" + req.body.month;
+    var cherry = "Cherry";
     const obj = []; 
-    obj[0] = {topping: "cherry", quantity: 2};
+    var cherryQuery = `SELECT SUM(QUANTITY) FROM ORDERS WHERE MONTH="${selectedMonth}" AND TOPPING="${cherry}";`;
+    obj[0] = {topping: "cherry", quantity: dbms_promise.dbquery(cherryQuery)};
     obj[1] = {topping: "plain", quantity: 6};
     obj[2] = {topping: "chocolate", quantity: 3};
 
